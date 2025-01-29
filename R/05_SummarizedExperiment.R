dd95ffce25f9c95d48349446b3158628a999d5f5
lcolladotor <lcolladotor@gmail.com>
2025-01-29 14:05:43 -0500

    Agregamos un stopifnot para demostrar dos formas identicas de hacer un subconjunto de un RSE a partir de muestras (columnas) del objeto

--- R/05_SummarizedExperiment.R
+++ R/05_SummarizedExperiment.R
@@ -64,6 +64,7 @@ rse[1:2, ]
 ## Comando 2
 rse[, c("A", "D", "F")]
 
+stopifnot(identical(rse[, c(1, 4, 6)], rse[, c("A", "D", "F")]))
 
 ## ----isee_basic, eval = FALSE---------------------------------
 # ## Explora el objeto rse de forma interactiva
