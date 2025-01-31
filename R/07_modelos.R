16be8a01a56b992e9ddca1a8514df3c06729d299
lcolladotor <lcolladotor@gmail.com>
2025-01-31 11:31:52 -0500

    Código de https://bioconductor.org/packages/release/bioc/vignettes/ExploreModelMatrix/inst/doc/ExploreModelMatrix.html#example-3

--- R/07_modelos.R
+++ R/07_modelos.R
@@ -91,6 +91,14 @@ vd <- VisualizeDesign(
 )
 cowplot::plot_grid(plotlist = vd$plotlist, ncol = 1)
 
+mat3 <- with(
+    sampleData,
+    model.matrix(~Response + Response:ind.n + Response:Treatment)
+)
+mat3
+
+colnames(mat3)
+
 ## ----EMM_example1_interactive, eval = FALSE-------------------
 # ## Usaremos shiny otra ves
 app <- ExploreModelMatrix::ExploreModelMatrix(
@@ -98,6 +106,37 @@ app <- ExploreModelMatrix::ExploreModelMatrix(
     designFormula = ~genotype + treatment
 )
 if (interactive()) shiny::runApp(app)
+
+## Ejemplo 3
+(
+    sampleData = data.frame(
+        condition = factor(
+            rep(c("ctrl_minus", "ctrl_plus", "ko_minus", "ko_plus"), 3)
+        ),
+        batch = factor(rep(1:6, each = 2))
+    )
+)
+#>     condition batch
+#> 1  ctrl_minus     1
+#> 2   ctrl_plus     1
+#> 3    ko_minus     2
+#> 4     ko_plus     2
+#> 5  ctrl_minus     3
+#> 6   ctrl_plus     3
+#> 7    ko_minus     4
+#> 8     ko_plus     4
+#> 9  ctrl_minus     5
+#> 10  ctrl_plus     5
+#> 11   ko_minus     6
+#> 12    ko_plus     6
+vd <- VisualizeDesign(
+    sampleData = sampleData,
+    designFormula = ~0 + batch + condition,
+    textSizeFitted = 4,
+    lineWidthFitted = 20,
+    dropCols = "conditionko_minus"
+)
+cowplot::plot_grid(plotlist = vd$plotlist, ncol = 1)
 
 ## ----download_SRP045638---------------------------------------
 library("recount3")
